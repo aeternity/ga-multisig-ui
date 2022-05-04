f21ea9406a01f64fa6ad4e9882b208e2d94b6303
janmichek <jan@janmichek.cz>
2022-05-04 15:33:47 +0200

    import dummy db

--- src/views/Load.vue
+++ src/views/Load.vue
@@ -1,6 +1,27 @@
 <template>
     <div class="about">
         <h1>Contracts where I am signer</h1>
+
+      <input
+        type="text"
+        v-model="todoName"
+        @keyup.enter="addTodo"
+        aria-label="Add a new Todo"
+        placeholder="Add a new Todo"
+      />
+
+      <ul>
+        <li
+          v-for="todo of todos"
+          :class="{ done: todo.done }"
+          :key="todo.id"
+          @click="doneTodo(todo.id)"
+        >
+          {{ todo.name }}
+        </li>
+      </ul>
+
+
       <div v-for="contract in myContracts">
         <div>
           {{ contract }}
@@ -24,16 +45,68 @@ import {  Universal, Node } from '@aeternity/aepp-sdk'
 
 import { loadMyContracts, updateContractInfo } from "../store"
 import { COMPILER_URL } from "../utils/aeternity/configs"
+import axios from "axios";
+
+const baseURL = "http://localhost:3001/todos";
+
+
 export default {
+  async created () {
+    try {
+      const res = await axios.get(baseURL);
+
+      this.todos = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+
+  },
+
   name: 'About',
   data: () => ({
     myContracts: null,
     contractDetail: null,
+    todoName: '',
+    todos:[]
   }),
   async mounted () {
     this.myContracts = await loadMyContracts()
   },
+
+
   methods: {
+    async doneTodo (id) {
+      try {
+        await axios.patch(`${baseURL}/${id}`, {
+          done: true
+        });
+
+        this.todos = this.todos.map(todo => {
+          if (todo.id === id) {
+            todo.done = true;
+          }
+
+          return todo;
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+
+
+    async addTodo () {
+      try {
+        const res = await axios.post(baseURL, { name: this.todoName });
+
+        this.todos = [...this.todos, res.data];
+
+        this.todoName = "";
+      } catch (e) {
+        console.error(e);
+      }
+    },
+
+
     async loadContractInfo (contract) {
       const signerSdk = await Universal({
         nodes: [{
@@ -51,3 +124,10 @@ export default {
   },
 }
 </script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+
+</style>
